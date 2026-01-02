78a49d7bf588660729397f2e820e01a64acfb4e1
kiranvmdy <kiranvmdy@gmail.com>
2026-01-02 19:48:43 +0530

    Fix navigation by removing router and using section anchors

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
 
 const queryClient = new QueryClient();
 
@@ -14,14 +11,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
+      <Index />
     </TooltipProvider>
   </QueryClientProvider>
 );
